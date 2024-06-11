e54e10d85b561ab48f581a85ec1b2a00972bef92
pepillo <pepecuadrajs@gmail.com>
2024-06-11 16:50:57 +0200

    entrega de ejercicios

---
+++ Ejercicio-4/sass/src/main.js
@@ -0,0 +1,9 @@
+var burguer = $(".burguer");
+var nav = $(".menu-nav");
+
+burguer.click(function() {
+    nav.toggleClass("display");
+    nav.css("overflow", "hidden");
+});
+
+//# sourceMappingURL=index.aa69868b.js.map
